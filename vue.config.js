f3b1bdab75ab9936293fb7073c3678b465865521
wuqiong <1506515805@qq.com>
2020-12-16 13:20:54 +0800

    change buildpath

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
-// const path = require('path')
-// module.exports = {
-//     build: {
-//         // Template for index.html
-//         index: path.resolve(__dirname, '../dist/index.html'),
-    
-//         // Paths
-//         assetsRoot: path.resolve(__dirname, '../dist'),
-//         assetsSubDirectory: 'static',
-//         assetsPublicPath: './'
-//     }
-// }
+const path = require('path')
+module.exports = {
+    publicPath:"./"
+}
